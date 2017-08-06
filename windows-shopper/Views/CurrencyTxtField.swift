eb196730816cbc719a0149c6bb6a8c48719d883f
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-08-05 23:28:35 -0400

    input accesoryview & indesignable

---
+++ windows-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  windows-shopper
+//
+//  Created by Sanjay Vekariya on 8/5/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
